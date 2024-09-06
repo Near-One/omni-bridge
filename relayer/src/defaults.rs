c62ba9e4409a365547b776ba05ab53f9d5613c5c
Ivan Frolov <frolvanya@gmail.com>
2024-09-06 01:58:34 -0400

    feat: moved basic implementation of eth events tracker to relayer

--- relayer/src/defaults.rs
+++ relayer/src/defaults.rs
@@ -1,6 +1,10 @@
 pub const SIGN_TRANSFER_GAS: u64 = 300_000_000_000_000;
 pub const SIGN_TRANSFER_ATTACHED_DEPOSIT: u128 = 500_000_000_000_000_000_000_000;
 
+/// Mainnet
+pub const ETH_RPC_MAINNET: &str = "https://eth.llamarpc.com";
+pub const BRIDGE_TOKEN_FACTORY_ADDRESS_MAINNET: &str = "0x252e87862A3A720287E7fd527cE6e8d0738427A2";
+
 /// Testnet
 pub const NEAR_RPC_TESTNET: &str = "https://rpc.testnet.near.org/";
 pub const ETH_RPC_TESTNET: &str = "https://ethereum-sepolia.blockpi.network/v1/rpc/public";
--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -1,8 +1,14 @@
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use near_jsonrpc_client::JsonRpcClient;
+
+use alloy::{
+    primitives::Address,
+    providers::{Provider, ProviderBuilder},
+    rpc::types::Filter,
+};
 
 mod defaults;
 mod startup;
@@ -13,11 +19,29 @@ mod utils;
 async fn main() -> Result<()> {
     pretty_env_logger::init();
 
+    // NEAR
     let client = JsonRpcClient::connect(defaults::NEAR_RPC_TESTNET);
     let near_signer = startup::near::create_signer()?;
     let connector = Arc::new(startup::build_connector(&near_signer)?);
 
-    startup::near::start_indexer(client, near_signer, connector).await?;
+    tokio::spawn(startup::near::start_indexer(client, near_signer, connector));
+
+    // ETH
+    let provider = ProviderBuilder::new().on_http(
+        defaults::ETH_RPC_MAINNET
+            .parse()
+            .context("Failed to parse ETH rpc provider as url")?,
+    );
+    let filter = Filter::new()
+        .address(defaults::BRIDGE_TOKEN_FACTORY_ADDRESS_MAINNET.parse::<Address>()?)
+        .event("Withdraw(string,address,uint256,string,address)")
+        .from_block(20085270)
+        .to_block(20085370);
+
+    let logs = provider.get_logs(&filter).await?;
+    println!("Logs: {:#?}", logs);
+
+    tokio::signal::ctrl_c().await?;
 
     Ok(())
 }
