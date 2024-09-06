7ae04b99707283238932465ab7bb228efbb57cb0
kiseln <3428059+kiseln@users.noreply.github.com>
2024-09-06 18:12:55 +0400

    Replaced scripts with hardhat tasks

--- evm/bridge-token-factory/hardhat.config.js
+++ evm/bridge-token-factory/hardhat.config.js
@@ -6,64 +6,17 @@ require("@nomicfoundation/hardhat-chai-matchers");
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY || '11'.repeat(32);
-const NEAR_RPC_URL = process.env.NEAR_RPC_URL;
-const NEAR_NETWORK = process.env.NEAR_NETWORK;
-const NEAR_TOKEN_LOCKER = process.env.NEAR_TOKEN_LOCKER;
-const NEAR_ON_ETH_CLIENT_ADDRESS = process.env.NEAR_ON_ETH_CLIENT_ADDRESS;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
-task('finish-deposit-ft', 'Deposit NEP-141 tokens on the Ethereum side')
-  .addParam('nearAccount', 'Near account id to get the proof')
-  .addParam('factory', 'The address of the eth factory contract')
-  .addParam('txReceiptId', 'Receipt id of the lock event on Near side')
-  .setAction(async (taskArgs) => {
-    const { findProof } = require('./utils/near-proof.js');
-    const proof = await findProof({
-      nearAccountId: taskArgs.nearAccount,
-      nearOnEthClientAddress: NEAR_ON_ETH_CLIENT_ADDRESS,
-      txReceiptId: taskArgs.txReceiptId,
-      receiverId: NEAR_TOKEN_LOCKER,
-      nearNodeUrl: NEAR_RPC_URL,
-      nearNetworkId: NEAR_NETWORK,
-    });
-
-    const BridgeTokenFactoryContract = await ethers.getContractFactory("BridgeTokenFactory");
-    const BridgeTokenFactory = BridgeTokenFactoryContract.attach(taskArgs.factory);
-    await BridgeTokenFactory.deposit(proof.borshProof, proof.proofBlockHeight);
-  });
-
 task('set-metadata-ft', 'Set metadata for NEP-141 tokens on the Ethereum side')
-  .addParam('nearAccount', 'Near account id to get the proof')
-  .addParam('factory', 'The address of the eth factory contract')
-  .addParam('txReceiptId', 'Receipt id of the lock event on Near side')
-  .setAction(async (taskArgs) => {
-    const { findProof } = require('./utils/near-proof.js');
-    const proof = await findProof({
-      nearAccountId: taskArgs.nearAccount,
-      nearOnEthClientAddress: NEAR_ON_ETH_CLIENT_ADDRESS,
-      txReceiptId: taskArgs.txReceiptId,
-      receiverId: NEAR_TOKEN_LOCKER,
-      nearNodeUrl: NEAR_RPC_URL,
-      nearNetworkId: NEAR_NETWORK,
-    });
-
-    const BridgeTokenFactoryContract = await ethers.getContractFactory("BridgeTokenFactory");
-    const BridgeTokenFactory = BridgeTokenFactoryContract.attach(taskArgs.factory);
-    await BridgeTokenFactory.setMetadata(proof.borshProof, proof.proofBlockHeight);
-  });
-
-task('new-token', 'Deploy new bridge token')
   .addParam('nearTokenAccount', 'Near account id of the token')
-  .addParam('factory', 'The address of the eth factory contract')
+  .addParam('name', 'The new name of the token')
+  .addParam('symbol', 'The new symbol of the token')
+  .addParam('factory', 'The address of the factory contract on Ethereum')
   .setAction(async (taskArgs) => {
     const BridgeTokenFactoryContract = await ethers.getContractFactory("BridgeTokenFactory");
     const BridgeTokenFactory = BridgeTokenFactoryContract.attach(taskArgs.factory);
-
-    console.log("Deploy new bridge token:", taskArgs.nearTokenAccount);
-    let tx = await BridgeTokenFactory.newBridgeToken(taskArgs.nearTokenAccount);
-    await tx.wait(5);
-    const tokenProxyAddress = await BridgeTokenFactory.nearToEthToken(taskArgs.nearTokenAccount);
-    console.log(`Token deployed at ${tokenProxyAddress}`);
+    await BridgeTokenFactory.setMetadata(taskArgs.nearTokenAccount, taskArgs.name, taskArgs.symbol);
   });
 
 task('add-token-to-whitelist-eth', 'Add a token to whitelist')
@@ -96,35 +49,79 @@ task('add-account-to-whitelist-eth', 'Add an account to whitelist')
     console.log("Tx hash", receipt.transactionHash);
   });
 
-task('withdraw-ft', 'Withdraw bridged tokens from the Ethereum side')
-  .addParam('factory', 'The address of the eth factory contract')
-  .addParam('token', 'Near token account id')
-  .addParam('bridgeTokenProxy', 'Bridged token address on eth')
-  .addParam('amount', 'Amount of tokens to withdraw')
-  .addParam('recipient', 'Near recipient account id ')
-  .setAction(async (taskArgs) => {
-    const { withdraw } = require('./utils/withdraw-ft.js');
-    await withdraw({
-      ethTokenFactoryAddress: taskArgs.factory,
-      token: taskArgs.token,
-      amount: taskArgs.amount,
-      recipient: taskArgs.recipient
-    });
+task("deploy-bridge-token-factory", "Deploys the BridgeTokenFactory contract")
+  .addParam("bridgeTokenImpl", "The address of the bridge token implementation")
+  .addParam("nearBridgeDerivedAddress", "The derived EVM address of the Near's OmniBridge")
+  .setAction(async (taskArgs, hre) => {
+    const { ethers, upgrades } = hre;
+
+    const BridgeTokenFactoryContract = 
+      await ethers.getContractFactory("BridgeTokenFactory");
+    const BridgeTokenFactory = await upgrades.deployProxy(
+      BridgeTokenFactoryContract,
+      [
+        taskArgs.bridgeTokenImpl,
+        taskArgs.nearBridgeDerivedAddress
+      ],
+      {
+        initializer: "initialize",
+        timeout: 0,
+      },
+    );
+
+    await BridgeTokenFactory.waitForDeployment();
+    console.log(`BridgeTokenFactory deployed at ${await BridgeTokenFactory.getAddress()}`);
+    console.log(
+      "Implementation address:",
+      await upgrades.erc1967.getImplementationAddress(
+        await BridgeTokenFactory.getAddress(),
+      ),
+    );
   });
 
-task('finish-withdraw-ft', 'Finish withdraw on near side')
-  .addParam('nearAccount', 'Near account id to submit the proof')
-  .addParam('locker', 'The address of the near locker contract')
-  .addParam('event', 'Lock event in json format {"transactionHash":"0x00", "logIndex":0}')
-  .setAction(async (taskArgs) => {
-    const { finishWithdraw } = require('./utils/withdraw-ft.js');
-    await finishWithdraw({
-      nearAccountId: taskArgs.nearAccount,
-      nearTokenLockerAccountId: taskArgs.locker,
-      lockedEvent: JSON.parse(taskArgs.event),
-      nearNodeUrl: NEAR_RPC_URL,
-      nearNetworkId: NEAR_NETWORK,
-    });
+task("deploy-token-impl", "Deploys the BridgeToken implementation")
+  .setAction(async () => {
+    const { ethers } = hre;
+
+    const BridgeTokenContractFactory = await ethers.getContractFactory("BridgeToken");
+    const BridgeTokenContract = await BridgeTokenContractFactory.deploy();
+    await BridgeTokenContract.waitForDeployment();
+    console.log(`BridgeTokenContract deployed at ${await BridgeTokenContract.getAddress()}`);
+  });
+
+task("upgrade-bridge-token", "Upgrades a BridgeToken to a new implementation")
+  .addParam("factory", "The address of the BridgeTokenFactory contract")
+  .addParam("nearTokenAccount", "The NEAR token ID")
+  .setAction(async (taskArgs, hre) => {
+    const { ethers } = hre;
+
+    const BridgeTokenFactoryContract = await ethers.getContractFactory("BridgeTokenFactory");
+    const BridgeTokenFactory = BridgeTokenFactoryContract.attach(taskArgs.factory);
+
+    console.log(`Upgrading token ${taskArgs.nearTokenAccount}`);
+    console.log(`Token proxy address:`, await BridgeTokenFactory.nearToEthToken(taskArgs.nearTokenAccount));
+
+    const BridgeTokenV2Instance = await ethers.getContractFactory("BridgeTokenV2");
+    const BridgeTokenV2 = await BridgeTokenV2Instance.deploy();
+    await BridgeTokenV2.waitForDeployment();
+
+    console.log(`BridgeTokenV2 deployed at ${await BridgeTokenV2.getAddress()}`);
+
+    const tx = await BridgeTokenFactory.upgradeToken(taskArgs.nearTokenAccount, await BridgeTokenV2.getAddress());
+    const receipt = await tx.wait();
+
+    console.log("Token upgraded at tx hash:", receipt.transactionHash);
+  });
+
+task("upgrade-factory", "Upgrades the BridgeTokenFactory contract")
+  .addParam("factory", "The address of the BridgeTokenFactory contract")
+  .setAction(async (taskArgs, hre) => {
+    const { ethers, upgrades } = hre;
+
+    const BridgeTokenFactoryContract = await ethers.getContractFactory("BridgeTokenFactory");
+    console.log("Current implementation address:", await upgrades.erc1967.getImplementationAddress(taskArgs.factory));
+    console.log("Upgrade factory, proxy address", taskArgs.factory);
+    await upgrades.upgradeProxy(taskArgs.factory, BridgeTokenFactoryContract);
   });
 
 task('etherscan-verify', 'Verify contract on etherscan')
