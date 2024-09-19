2f6ead7b4219ce5709be516742c71ca5f021f8b4
Ivan Frolov <frolvanya@gmail.com>
2024-09-18 22:38:00 -0400

    chore: made clippy happy

--- omni-relayer/src/utils/mod.rs
+++ omni-relayer/src/utils/mod.rs
@@ -4,10 +4,8 @@ pub mod near;
 pub mod redis;
 
 pub async fn get_price(symbol: &str) -> Option<f64> {
-    let url = format!(
-        "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd",
-        symbol
-    );
+    let url =
+        format!("https://api.coingecko.com/api/v3/simple/price?ids={symbol}&vs_currencies=usd");
 
     let response = reqwest::get(&url).await.ok()?;
     let json: HashMap<String, HashMap<String, f64>> = response.json().await.ok()?;
