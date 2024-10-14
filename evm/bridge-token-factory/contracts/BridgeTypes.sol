bd8df646d6770b52e8d641ca3dce9086c955cce9
karim-en <karim.alabtakh@nearone.org>
2024-10-14 23:04:05 +0100

    feat: basic locker implementation

---
+++ evm/bridge-token-factory/contracts/BridgeTypes.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.24;
+
+library BridgeTypes {
+    struct FinTransferPayload {
+        uint128 nonce;
+        string token;
+        uint128 amount;
+        address recipient;
+        string feeRecipient;
+    }
+
+    struct MetadataPayload {
+        string token;
+        string name;
+        string symbol;
+        uint8 decimals;
+    }
+
+    struct ClaimFeePayload {
+        uint128[] nonces;
+        uint128 amount;
+        address recipient;
+    }
+
+    struct UnlockTokenPayload {
+        uint128 nonce;
+        address token;
+        uint128 amount;
+        address recipient;
+        string feeRecipient;
+    }
+
+        event InitTransfer(
+        address indexed sender,
+        address indexed tokenAddress,
+        uint128 indexed nonce,
+        string token,
+        uint128 amount,
+        uint128 fee,
+        uint128 nativeFee,
+        string recipient
+    );
+
+    event FinTransfer(
+        uint128 indexed nonce,
+        string token,
+        uint128 amount,
+        address recipient,
+        string feeRecipient
+    );
+
+    event DeployToken(
+        address indexed tokenAddress,
+        string token,
+        string name,
+        string symbol,
+        uint8 decimals
+    );
+
+    event SetMetadata(
+        address indexed tokenAddress,
+        string token,
+        string name,
+        string symbol,
+        uint8 decimals
+    );
+
+    event Locked(
+        uint128 nonce,
+        address indexed token,
+        address indexed sender,
+        uint128 amount,
+        uint128 fee,
+        uint128 nativeFee,
+        string accountId,
+        string msg
+    );
+
+    event Unlocked(
+        uint128 indexed nonce,
+        address indexed token,
+        uint128 amount,
+        address recipient,
+        string feeRecipient
+    );
+}
