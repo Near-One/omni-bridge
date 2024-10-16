0803f3ec38d7ad52f4b841973b7665055dcdaf8f
Ivan Frolov <frolvanya@gmail.com>
2024-10-16 13:59:29 +0100

    refactor: removed optional typing

--- omni-relayer/src/config.rs
+++ omni-relayer/src/config.rs
@@ -1,8 +1,29 @@
-use anyhow::{Context, Result};
-
 use alloy::primitives::Address;
 use near_primitives::{borsh::BorshDeserialize, types::AccountId};
 use omni_types::{evm::utils::keccak256, OmniAddress, H160};
+
+fn derive_evm_address_from_private_key() -> OmniAddress {
+    let decoded_private_key = hex::decode(
+        std::env::var("ETH_PRIVATE_KEY").expect("Failed to get `ETH_PRIVATE_KEY` env variable"),
+    )
+    .expect("Failed to decode `ETH_PRIVATE_KEY`");
+
+    let secret_key = secp256k1::SecretKey::from_slice(&decoded_private_key)
+        .expect("Failed to create a `SecretKey` from the provided private key");
+
+    let secp = secp256k1::Secp256k1::new();
+    let public_key = secp256k1::PublicKey::from_secret_key(&secp, &secret_key);
+    let public_key_bytes = public_key.serialize_uncompressed();
+    let public_key_bytes = &public_key_bytes[1..];
+
+    let result = keccak256(public_key_bytes);
+    let address = &result[12..];
+
+    OmniAddress::Eth(
+        H160::try_from_slice(address)
+            .expect("Failed to create `OmniAddress` from the derived public key"),
+    )
+}
 
 #[derive(Debug, Clone, serde::Deserialize)]
 pub struct Config {
@@ -29,7 +50,9 @@ pub struct Near {
     pub rpc_url: String,
     pub token_locker_id: AccountId,
     pub credentials_path: Option<String>,
-    pub relayer_address_on_evm: Option<OmniAddress>,
+
+    #[serde(default = "derive_evm_address_from_private_key")]
+    pub relayer_address_on_evm: OmniAddress,
 }
 
 #[derive(Debug, Clone, serde::Deserialize)]
@@ -39,36 +62,4 @@ pub struct Evm {
     pub chain_id: u64,
     pub bridge_token_factory_address: Address,
     pub block_processing_batch_size: u64,
-}
-
-impl Config {
-    pub fn new(config_path: String) -> Result<Self> {
-        let mut config = toml::from_str::<Self>(&std::fs::read_to_string(config_path)?)?;
-
-        if config.near.relayer_address_on_evm.is_none() {
-            let decoded_private_key = hex::decode(
-                std::env::var("ETH_PRIVATE_KEY")
-                    .context("Failed to get `ETH_PRIVATE_KEY` env variable")?,
-            )
-            .context("Failed to decode `ETH_PRIVATE_KEY`")?;
-            let secret_key = secp256k1::SecretKey::from_slice(&decoded_private_key)
-                .context("Failed to construct a `SecretKey` from given `ETH_PRIVATE_KEY`")?;
-
-            let secp = secp256k1::Secp256k1::new();
-            let public_key = secp256k1::PublicKey::from_secret_key(&secp, &secret_key);
-            let public_key_bytes = public_key.serialize_uncompressed();
-            let public_key_bytes = &public_key_bytes[1..];
-
-            let result = keccak256(public_key_bytes);
-
-            let address = &result[12..];
-
-            config.near.relayer_address_on_evm = Some(OmniAddress::Eth(
-                H160::try_from_slice(address)
-                    .context("Failed to construct an `OmniAddress` from derived address")?,
-            ));
-        }
-
-        Ok(config)
-    }
 }
