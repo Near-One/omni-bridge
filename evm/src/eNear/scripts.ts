d4aa3121eb22dce470167301883406a805e0cf56
r-near <163825889+r-near@users.noreply.github.com>
2024-11-25 15:44:30 -0800

    TypeScript Migration & CI Improvements for EVM Package (#129)
    * Convert to TypeScript
    
    * Compile hardhat
    
    * Test cleanup
    
    * Linting
    
    * Fix all broken tests, support dynamic signatures
    
    * Package updates and cleanup
    
    * Use existing types
    
    * File cleanup
    
    * Fix types
    
    * Don't run Rust CI unless there are Rust changes
    
    * Renaming some things
    
    * Lockfile
    
    * Fix types
    
    * Linting
    
    * More Linting
    
    * Package updates
    
    * Linting
    
    * Prevent duplicate workflows
    
    * Remove extra file

---
+++ evm/src/eNear/scripts.ts
@@ -0,0 +1,48 @@
+import { task } from "hardhat/config"
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
+
+task("deploy-e-near-proxy", "Deploys the ENearProxy contract")
+  .addParam("enear", "Address of eNear contract")
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { ethers, upgrades } = hre
+
+    const eNear = await ethers.getContractAt("IENear", taskArgs.enear)
+    const nearConnector = await eNear.nearConnector()
+
+    const eNearProxyContract = await ethers.getContractFactory("ENearProxy")
+    const eNearProxy = await upgrades.deployProxy(
+      eNearProxyContract,
+      [taskArgs.enear, nearConnector, 0],
+      {
+        initializer: "initialize",
+        timeout: 0,
+      },
+    )
+
+    await eNearProxy.waitForDeployment()
+    console.log(`eNearProxy deployed at ${await eNearProxy.getAddress()}`)
+    console.log(
+      "Implementation address:",
+      await upgrades.erc1967.getImplementationAddress(await eNearProxy.getAddress()),
+    )
+  })
+
+task("e-near-set-admin", "Set the proxy as admin for eNear")
+  .addParam("newAdmin", "New admin address")
+  .addParam("enear", "Address of the eNear contract")
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { ethers } = hre
+    const eNear = await ethers.getContractAt("IENear", taskArgs.enear)
+    await eNear.adminSstore(9, ethers.zeroPadValue(taskArgs.newAdmin, 32))
+  })
+
+task("deploy-fake-prover", "Deploy fake prover").setAction(
+  async (_taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { ethers } = hre
+    const FakeProverContractFactory = await ethers.getContractFactory("FakeProver")
+    const FakeProverContract = await FakeProverContractFactory.deploy()
+    await FakeProverContract.waitForDeployment()
+
+    console.log(`FakeProver deployed at ${await FakeProverContract.getAddress()}`)
+  },
+)
