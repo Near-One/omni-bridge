3d16aed2735bc92356a358f7e0bf71a210c66d3f
Karim <karim.alabtakh@nearone.org>
2024-12-25 17:18:38 +0000

    Fix: fix fin transfer for EVM native token. (#166)
    * Fix fin transfer for EVM native token.
    
    * Use custome error instead of string

--- evm/src/omni-bridge/contracts/OmniBridge.sol
+++ evm/src/omni-bridge/contracts/OmniBridge.sol
@@ -42,6 +42,7 @@ contract OmniBridge is
     error InvalidSignature();
     error NonceAlreadyUsed(uint64 nonce);
     error InvalidFee();
+    error FailedToSendEther();
 
     function initialize(
         address tokenImplementationAddress_,
@@ -194,7 +195,11 @@ contract OmniBridge is
             revert InvalidSignature();
         }
 
-        if (customMinters[payload.tokenAddress] != address(0)) {
+        if (payload.tokenAddress == address(0)) {
+            (bool success, ) = payload.recipient.call{value: payload.amount}("");
+            if (!success) revert FailedToSendEther();
+        }
+        else if (customMinters[payload.tokenAddress] != address(0)) {
             ICustomMinter(customMinters[payload.tokenAddress]).mint(payload.tokenAddress, payload.recipient, payload.amount);
         } else if (isBridgeToken[payload.tokenAddress]) {
             BridgeToken(payload.tokenAddress).mint(payload.recipient, payload.amount);
