6a9a646720e1def63d466368c6dc6a2edd3996da
Ivan Frolov <frolvanya@gmail.com>
2025-01-18 22:46:02 -0500

    feat: handle gas estimation error

--- omni-relayer/src/workers/evm.rs
+++ omni-relayer/src/workers/evm.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use bridge_connector_common::result::BridgeSdkError;
 use futures::future::join_all;
 use log::{info, warn};
 
@@ -269,7 +270,17 @@ async fn handle_init_transfer_event(
             )
             .await;
         }
-        Err(err) => warn!("Failed to finalize InitTransfer: {}", err),
+        Err(err) => {
+            if let BridgeSdkError::EvmGasEstimateError(_) = err {
+                utils::redis::remove_event(
+                    &mut redis_connection,
+                    utils::redis::EVM_INIT_TRANSFER_EVENTS,
+                    &key,
+                )
+                .await;
+            }
+            warn!("Failed to finalize InitTransfer: {}", err);
+        }
     }
 
     if current_timestamp - init_transfer_with_timestamp.creation_timestamp
