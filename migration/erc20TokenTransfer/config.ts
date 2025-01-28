ab5e20efbd67e493f3294a591188015f8dc28c86
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-01-28 11:34:23 +0200

    remove test data

--- migration/erc20TokenTransfer/config.ts
+++ migration/erc20TokenTransfer/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     "chainId":  11155111n,
     "network": "sepolia",
-    "safe_address": "0x816706a90D1067d41D557AF1543308F801c4256f",
-    "erc20_locker": "0xbe84af9054e9a284FC237d1CB03Adc9a6c40cD94",
-    "omni_locker": "0x35a461C0d073db46AC8450BA7De17e96c213f49d",
+    "safe_address": "",
+    "erc20_locker": "",
+    "omni_locker": "",
 };
--- migration/erc20TokenTransfer/tokensList.ts
+++ migration/erc20TokenTransfer/tokensList.ts
@@ -1,5 +1,1 @@
-export const tokens_list = [
-"7a7a1dc7665912322016149283e7ae045caf79d8",
-"17ea5dcdc18787dbfd04ca947e77c81d94f7b2c5",
-"ffd797ebbfa626fe7c1eb9cc05bed04a55b814ae"
-];
+export const tokens_list = [];
