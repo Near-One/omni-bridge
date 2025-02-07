c1ffc6ee1a8c6f5f4951407d842ab86a33388283
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-02-07 21:54:35 +0200

    use .env instead config

--- migration/erc20TokenTransfer/initTransfer.ts
+++ migration/erc20TokenTransfer/initTransfer.ts
@@ -1,20 +1,13 @@
 import { connect, keyStores, Contract } from "near-api-js";
 import { ethers, JsonRpcProvider, parseUnits, Interface } from 'ethers'
-import EthersAdapter from '@safe-global/protocol-kit'
 import dotenv from 'dotenv'
 import SafeApiKit from '@safe-global/api-kit'
 import Safe from '@safe-global/protocol-kit'
-import SafeFactory from '@safe-global/protocol-kit'
-import { SafeAccountConfig } from '@safe-global/protocol-kit'
-import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types'
 import {
   MetaTransactionData,
   OperationType
 } from '@safe-global/types-kit'
 import Web3 from 'web3';
-
-
-import { config } from './config'
 
 const erc20BalanceOfAbi = [
   {
@@ -29,11 +22,11 @@ const erc20BalanceOfAbi = [
 async function initNear() {
   const keyStore = new keyStores.InMemoryKeyStore();
   const near_config = {
-    networkId: config.near_network_id,
+    networkId: process.env.NETWORK_NEAR!,
     keyStore,
-    nodeUrl: `https://rpc.${config.near_network_id}.near.org`,
-    walletUrl: `https://wallet.${config.near_network_id}.near.org`,
-    helperUrl: `https://helper.${config.near_network_id}.near.org`
+    nodeUrl: `https://rpc.${process.env.NETWORK_NEAR!}.near.org`,
+    walletUrl: `https://wallet.${process.env.NETWORK_NEAR!}.near.org`,
+    helperUrl: `https://helper.${process.env.NETWORK_NEAR!}.near.org`
   };
 
   return await connect(near_config);
@@ -41,19 +34,19 @@ async function initNear() {
 
 async function getTotalSupply(token_id: string) {
     console.log(token_id)
-    
+
     const near = await initNear();
-    const account = await near.account("script_account.near"); 
-    
+    const account = await near.account("script_account.near");
+
     const contract = new Contract(account, token_id, {
       viewMethods: ["ft_total_supply"],
       changeMethods: [],
-      useLocalViewExecution: true          
+      useLocalViewExecution: true
     });
 
     const result = await account.viewFunction({
-        contractId:  token_id, 
-        methodName: "ft_total_supply", 
+        contractId:  token_id,
+        methodName: "ft_total_supply",
         args: {}});
 
     return result
@@ -61,17 +54,17 @@ async function getTotalSupply(token_id: string) {
 
 async function extractTokensList() {
     const near = await initNear();
-    const account = await near.account("script_account.near"); 
-    
-    const contract = new Contract(account, config.bridge_factory, {
+    const account = await near.account("script_account.near");
+
+    const contract = new Contract(account, process.env.BRIDGE_TOKEN_FACTORY_ACCOUNT_ID!, {
       viewMethods: ["get_tokens"],
       changeMethods: [],
-      useLocalViewExecution: true          
+      useLocalViewExecution: true
     });
 
     const result = await account.viewFunction({
-        contractId:  config.bridge_factory, 
-        methodName: "get_tokens", 
+        contractId: process.env.BRIDGE_TOKEN_FACTORY_ACCOUNT_ID!,
+        methodName: "get_tokens",
         args: {}});
 
     return result
@@ -80,36 +73,36 @@ async function extractTokensList() {
 (async () => {
     dotenv.config()
 
-    const RPC_URL=`https://` + config.network + `.infura.io/v3/` + process.env.INFURA_API_KEY!
+    const RPC_URL=`https://` + process.env.NETWORK_ETH! + `.infura.io/v3/` + process.env.INFURA_API_KEY!
     const web3 = new Web3(RPC_URL);
 
     const provider = new JsonRpcProvider(RPC_URL)
 
     const owner1Signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
     const apiKit = new SafeApiKit({
-        chainId: config.chainId
+        chainId: process.env.CHAIN_ID!
     })
-    
+
     const protocolKitOwner1 = await Safe.init({
         provider: RPC_URL,
         signer: process.env.PRIVATE_KEY!,
-        safeAddress: config.safe_address
+        safeAddress: process.env.SAFE_ADDRESS!
     })
 
-    const destination = config.erc20_locker;
-    
+    const destination = process.env.ERC20_LOCKER!;
+
     const erc20Abi = ["function adminTransfer(address,address,uint256)", "function balanceOf(address) view returns (uint256)"];
     const erc20Interface = new Interface(erc20Abi);
-    
+
     const tokens_list = await extractTokensList();
     console.log(tokens_list)
     const txs = [];
     for (let i = 0; i < tokens_list.length; i++) {
-        const contract = new web3.eth.Contract(erc20BalanceOfAbi, tokens_list[i]);    
+        const contract = new web3.eth.Contract(erc20BalanceOfAbi, tokens_list[i]);
         const balance = await contract.methods.balanceOf(destination).call();
-        const total_supply = await getTotalSupply(tokens_list[i] + "." + config.bridge_factory);
+        const total_supply = await getTotalSupply(tokens_list[i] + "." + process.env.BRIDGE_TOKEN_FACTORY_ACCOUNT_ID!);
         console.log("ERC20 token: ", tokens_list[i], ", balance=", balance, "total_supply=", total_supply);
-        const data = erc20Interface.encodeFunctionData("adminTransfer", [tokens_list[i], config.omni_locker, total_supply]);
+        const data = erc20Interface.encodeFunctionData("adminTransfer", [tokens_list[i], process.env.OMNI_LOCKER!, total_supply]);
 
         const safeTransactionData: MetaTransactionData = {
             to: destination,
@@ -117,10 +110,10 @@ async function extractTokensList() {
             data: data,
             operation: OperationType.Call
         };
-        
+
         txs.push(safeTransactionData);
     }
-    
+
     const safeTransaction = await protocolKitOwner1.createTransaction({
         transactions: txs
     })
@@ -130,12 +123,12 @@ async function extractTokensList() {
 
     // Propose transaction to the service
     await apiKit.proposeTransaction({
-        safeAddress: config.safe_address,
+        safeAddress: process.env.SAFE_ADDRESS!,
         safeTransactionData: safeTransaction.data,
         safeTxHash,
         senderAddress: owner1Signer.address,
         senderSignature: signature.data
     })
-    
+
     console.log(safeTxHash);
 })()
