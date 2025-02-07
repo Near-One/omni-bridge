b0970debe8287077aa061865b18ce09b19219576
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-02-07 22:01:38 +0200

    config -> env

--- migration/checkERC20EthToNearTransfersFinalization/index.js
+++ migration/checkERC20EthToNearTransfersFinalization/index.js
@@ -5,18 +5,16 @@ import { Header } from 'eth-object';
 import { ethers } from "ethers";
 import { Formatter } from "@ethersproject/providers";
 
-import { config } from "./config.js"
-
 dotenv.config();
 
 const ABI = [
   "event Locked(address indexed token, address indexed sender, uint256 amount, string accountId)"
 ];
 
-const provider = new ethers.JsonRpcProvider(`https://${config.eth_network_id}.infura.io/v3/${process.env.INFURA_API_KEY}`);
-const contract = new ethers.Contract(config.erc20_locker_address, ABI, provider);
+const provider = new ethers.JsonRpcProvider(`https://${process.env.NETWORK_ETH}.infura.io/v3/${process.env.INFURA_API_KEY}`);
+const contract = new ethers.Contract(process.env.ERC20_LOCKER, ABI, provider);
 
-const schema = { struct: { 
+const schema = { struct: {
       log_index: "u64",
       log_entry_data: { array: { type: 'u8' }},
       receipt_index: "u64",
@@ -27,11 +25,11 @@ const schema = { struct: {
 async function initNear() {
   const keyStore = new keyStores.InMemoryKeyStore();
   const near_config = {
-    networkId: config.near_network_id,
+    networkId: process.env.NETWORK_NEAR,
     keyStore,
-    nodeUrl: `https://rpc.${config.near_network_id}.near.org`,
-    walletUrl: `https://wallet.${config.near_network_id}.near.org`,
-    helperUrl: `https://helper.${config.near_network_id}.near.org`
+    nodeUrl: `https://rpc.${process.env.NETWORK_NEAR}.near.org`,
+    walletUrl: `https://wallet.${process.env.NETWORK_NEAR}.near.org`,
+    helperUrl: `https://helper.${process.env.NETWORK_NEAR}.near.org`
   };
 
   return await connect(near_config);
@@ -43,17 +41,17 @@ function bytesBorshStringify(input) {
 
 async function isUsedProof(proof) {
     const near = await initNear();
-    const account = await near.account("script_account.near"); 
-    
-    const contract = new Contract(account, config.token_factory_account_id, {
+    const account = await near.account("script_account.near");
+
+    const contract = new Contract(account, process.env.BRIDGE_TOKEN_FACTORY_ACCOUNT_ID, {
       viewMethods: ["is_used_proof"],
-      changeMethods: []          
+      changeMethods: []
     });
 
     const result = await account.viewFunction({
-        contractId:  config.token_factory_account_id, 
-        methodName: "is_used_proof", 
-        args: proof, 
+        contractId: process.env.BRIDGE_TOKEN_FACTORY_ACCOUNT_ID,
+        methodName: "is_used_proof",
+        args: proof,
         stringify: bytesBorshStringify});
 
     return result
@@ -62,7 +60,7 @@ async function isUsedProof(proof) {
 
 async function getLatestEvents() {
     const latestBlock = await provider.getBlockNumber();
-    const fromBlock = latestBlock - 3000; 
+    const fromBlock = latestBlock - 3000;
 
     const events = await contract.queryFilter("Locked", fromBlock, latestBlock);
 
@@ -74,14 +72,14 @@ async function getLatestEvents() {
         'eth_getBlockByNumber',
         [ethers.toBeHex(event.blockNumber), false]);
       const header_rlp = Header.fromRpc(block).serialize();
-     
+
       const rpcObjFormatter = new Formatter();
       const receipt = rpcObjFormatter.receipt(await provider.send('eth_getTransactionReceipt', [event.transactionHash]));
-     
+
       const logIndexInArray = receipt.logs.findIndex(
          l => l.logIndex == event.index
       );
-      
+
       const proofLight = {
           "log_index": logIndexInArray,
           "log_entry_data": [],
@@ -90,10 +88,10 @@ async function getLatestEvents() {
           "header_data": header_rlp,
           "proof": [],
       }
-      
+
       const serializedProof = borsh.serialize(schema, proofLight);
       const res = await isUsedProof(serializedProof);
-      
+
       if (res == false) {
          cnt_not_fin += 1;
          console.log("Transaction ", event.transactionHash, " is NOT finalize");
@@ -101,9 +99,9 @@ async function getLatestEvents() {
          console.log("Transaction ", event.transactionHash, " is finalize");
       }
     }
-    
+
     console.log();
-    
+
     if (cnt_not_fin > 0) {
         console.log(cnt_not_fin, " transactions are NOT finalize, wait for finalization!");
     } else {
