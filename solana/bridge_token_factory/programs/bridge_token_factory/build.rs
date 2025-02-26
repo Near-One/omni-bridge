a3a40cd8053bb4878a6d108808ada34d3f621bc9
kiseln <3428059+kiseln@users.noreply.github.com>
2025-02-26 12:39:33 +0530

    test for explorer verification

--- solana/bridge_token_factory/programs/bridge_token_factory/build.rs
+++ solana/bridge_token_factory/programs/bridge_token_factory/build.rs
@@ -3,8 +3,7 @@ use std::{env, fs, path::Path};
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    let program_id =
-        env::var("PROGRAM_ID").unwrap();
+    let program_id = env::var("PROGRAM_ID").unwrap_or("Gy1XPwYZURfBzHiGAxnw3SYC33SfqsEpGSS5zeBge28p".to_string());
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("program_id.rs");
