67e27c744d98e8b8a6704eb18c0bb180a9579b71
Ivan Frolov <frolvanya@gmail.com>
2025-03-03 21:55:26 -0500

    fix: replaced `as` conversion with `from`

--- omni-relayer/src/utils/fee.rs
+++ omni-relayer/src/utils/fee.rs
@@ -40,11 +40,12 @@ pub async fn is_fee_sufficient(
 
     match (native_fee, transferred_fee) {
         (0, 0) => anyhow::bail!("No fee information found"),
-        (0, fee) if fee > 0 => Ok(
-            provided_fee.fee.0 >= fee * (100 - config.bridge_indexer.fee_discount) as u128 / 100
-        ),
+        (0, fee) if fee > 0 => {
+            Ok(provided_fee.fee.0
+                >= fee * u128::from(100 - config.bridge_indexer.fee_discount) / 100)
+        }
         (native_fee, 0) if native_fee > 0 => Ok(provided_fee.native_fee.0
-            >= native_fee * (100 - config.bridge_indexer.fee_discount) as u128 / 100),
+            >= native_fee * u128::from(100 - config.bridge_indexer.fee_discount) / 100),
         (native_fee, fee) => Ok(U256::from(provided_fee.fee.0) * U256::from(native_fee)
             + U256::from(provided_fee.native_fee.0) * U256::from(fee)
             >= U256::from(fee)
