d8bfae425d13ce8c83679e8bf647b4eb28e1b87d
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-03-06 16:21:40 +0000

    script for add custom tokens without multisig

---
+++ migration/addCustomToken/add_custom_tokens.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source .env
+
+IFS=' ' read -r -a tokens <<< "$TOKENS_LIST"
+
+for near_token in "${tokens[@]}"; do
+  echo "Near Token: $near_token"
+
+  token_address=$(cast call $BRIDGE_TOKEN_FACTORY "nearToEthToken(string)(address)" "$near_token" --rpc-url $RPC_URL)
+  echo "Token Ethereum Address: $token_address"
+
+  if [[ -z "$token_address" || "$token_address" == "0x0000000000000000000000000000000000000000" ]]; then
+    echo "No Address fetched for $near_token"
+    continue
+  fi
+
+  decimals=$(cast call $token_address "decimals()(uint8)" --rpc-url $RPC_URL)
+  echo "Decimals for token: $decimals"
+
+  if [[ -z "$decimals" ]]; then
+    echo "Error: no decimals found $token_address"
+    continue
+  fi
+
+  cast send "$OMNI_BRIDGE_ETH" "addCustomToken(string,address,address,uint8)" \
+    "$near_token" "$token_address" "$BRIDGE_TOKEN_FACTORY" "$decimals" --private-key "$OMNI_BRIDGE_ADMIN_PRIVATE_KEY" --rpc-url $RPC_URL
+
+done
+
