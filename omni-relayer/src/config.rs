1d48e33c5fa4e806c1cce190336699d8962ffd11
Ivan Frolov <frolvanya@gmail.com>
2025-03-11 18:20:13 -0400

    fix: check fee before finalizing transfer

--- omni-relayer/src/config.rs
+++ omni-relayer/src/config.rs
@@ -3,7 +3,7 @@ use alloy::{
     signers::{k256::ecdsa::SigningKey, local::LocalSigner},
 };
 use near_primitives::types::AccountId;
-use omni_types::ChainKind;
+use omni_types::{ChainKind, OmniAddress};
 use serde::Deserialize;
 
 pub fn get_private_key(chain_kind: ChainKind) -> String {
@@ -94,6 +94,7 @@ pub struct Near {
     pub rpc_url: String,
     pub token_locker_id: AccountId,
     pub credentials_path: Option<String>,
+    pub sign_without_checking_fee: Option<Vec<OmniAddress>>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
