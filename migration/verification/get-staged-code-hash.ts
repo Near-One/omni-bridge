536b9230045e6c4d7bd55214ade4b898857d3b8f
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2025-03-27 15:28:42 +0000

    feat: add stage code hash getter (#304)
    * feat: add stage code hash getter
    
    * chore: show bs58 hash as well

---
+++ migration/verification/get-staged-code-hash.ts
@@ -0,0 +1,58 @@
+import { connect, keyStores } from "near-api-js";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import { Buffer } from "buffer";
+import bs58 from "bs58";
+
+dotenv.config();
+
+interface NearConfig {
+	networkId: string;
+	nodeUrl: string;
+}
+
+async function get_staged_code_hash(config: NearConfig): Promise<void> {
+	const contractId = process.env.OMNI_BRIDGE_ACCOUNT_ID || ""
+
+	if (!contractId) {
+		console.log(chalk.yellow(`${contractId} ID not provided, skipping...`));
+		return;
+	}
+
+
+	const keyStore = new keyStores.InMemoryKeyStore();
+	const nearConnection = await connect({
+		networkId: config.networkId,
+		keyStore,
+		nodeUrl: config.nodeUrl,
+		headers: {},
+	});
+	const account = await nearConnection.account("dummy.near");
+
+	try {
+		const up_staged_code_hash = await account.viewFunction({
+			contractId,
+			methodName: "up_staged_code_hash",
+			args: {},
+		});
+		const buffer = Buffer.from(up_staged_code_hash);
+		const base64Encoded = buffer.toString("base64");
+
+		console.log("Base64:", base64Encoded);
+
+		const base58Encoded = bs58.encode(buffer);
+		console.log("Base58:", base58Encoded);
+	} catch (e) {
+		console.log(
+			chalk.yellow(
+				`up_staged_code_hash function not found or failed`,
+			),
+		);
+	}
+}
+
+const nearConfig: NearConfig = {
+	networkId: process.env.NETWORK_NEAR || "testnet",
+	nodeUrl: `https://rpc.${process.env.NETWORK_NEAR || "testnet"}.near.org`,
+};
+get_staged_code_hash(nearConfig)
