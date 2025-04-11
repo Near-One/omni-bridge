3662269ae0bd92fafae19767d57de048fc8d8b8c
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2025-04-11 16:18:01 -0400

    chore: lowercase and title-case serialization (#308)
    * chore: lowercase and title-case serialization
    
    * chore: leave title-case as a primary way to serialize

--- near/omni-types/src/lib.rs
+++ near/omni-types/src/lib.rs
@@ -144,10 +144,15 @@ impl Serialize for H160 {
 #[repr(u8)]
 pub enum ChainKind {
     #[default]
+    #[serde(alias = "eth")]
     Eth,
+    #[serde(alias = "near")]
     Near,
+    #[serde(alias = "sol")]
     Sol,
+    #[serde(alias = "arb")]
     Arb,
+    #[serde(alias = "base")]
     Base,
 }
 
