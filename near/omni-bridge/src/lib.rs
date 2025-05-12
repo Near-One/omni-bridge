a5fdc86e1a476c1fe94b8625320f46cd529bc3ef
karim-en <karim.alabtakh@nearone.org>
2025-05-12 14:20:52 +0100

    Remove DAO requirement for decimals update

--- near/omni-bridge/src/lib.rs
+++ near/omni-bridge/src/lib.rs
@@ -1012,23 +1012,33 @@ impl Contract {
     #[access_control_any(roles(Role::DAO, Role::MetadataManager))]
     pub fn set_token_metadata(
         &mut self,
-        token: AccountId,
+        address: OmniAddress,
         name: Option<String>,
         symbol: Option<String>,
         icon: Option<String>,
         reference: Option<String>,
         reference_hash: Option<Base64VecU8>,
-        decimals: Option<u8>,
     ) -> Promise {
-        // Only DAO can set decimals
-        require!(
-            decimals.is_none()
-                || self.acl_has_role(Role::DAO.into(), env::predecessor_account_id())
-        );
+        let token = self.get_token_id(&address);
+        require!(self.deployed_tokens.contains(&token));
+
+        let decimals = self
+            .token_decimals
+            .get(&address)
+            .sdk_expect("ERR_TOKEN_DECIMALS_NOT_FOUND")
+            .decimals;
+        require!(decimals > 0, "ERR_INVALID_DECIMALS");
 
         ext_token::ext(token)
             .with_static_gas(SET_METADATA_GAS)
-            .set_metadata(name, symbol, reference, reference_hash, decimals, icon)
+            .set_metadata(
+                name,
+                symbol,
+                reference,
+                reference_hash,
+                Some(decimals),
+                icon,
+            )
     }
 
     pub fn get_current_destination_nonce(&self, chain_kind: ChainKind) -> Nonce {
