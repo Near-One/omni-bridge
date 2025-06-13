c3a6408fb4986609ff56446eaaf0a596ca0116ea
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-06-13 22:23:32 +0100

    UTXO json serialization

--- near/omni-types/src/btc.rs
+++ near/omni-types/src/btc.rs
@@ -1,6 +1,6 @@
 use bitcoin::{OutPoint, TxOut};
-use serde::{Deserialize, Serialize};
-use serde_with::{serde_as, DisplayFromStr};
+use near_sdk::near;
+use serde::{self, Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum TokenReceiverMessage {
@@ -12,12 +12,33 @@ pub enum TokenReceiverMessage {
     },
 }
 
-#[serde_as]
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[near(serializers=[json])]
+#[derive(Debug, Clone)]
 pub struct UTXO {
     pub path: String,
     pub tx_bytes: Vec<u8>,
     pub vout: usize,
-    #[serde_as(as = "DisplayFromStr")]
+    #[serde(with = "u64_dec_format")]
     pub balance: u64,
+}
+
+pub mod u64_dec_format {
+    use near_sdk::serde::de;
+    use near_sdk::serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(num: &u64, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        serializer.serialize_str(&num.to_string())
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<u64, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        String::deserialize(deserializer)?
+            .parse()
+            .map_err(de::Error::custom)
+    }
 }
