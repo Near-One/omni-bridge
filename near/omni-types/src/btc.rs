a517f6b8fc74b9da5e8ee4db4933270d99546d6f
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-06-16 09:40:24 +0100

    fix utxo

--- near/omni-types/src/btc.rs
+++ near/omni-types/src/btc.rs
@@ -1,26 +1,6 @@
 use bitcoin::{OutPoint, TxOut};
 use near_sdk::near;
-use serde::{self, Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum TokenReceiverMessage {
-    DepositProtocolFee,
-    Withdraw {
-        target_btc_address: String,
-        input: Vec<OutPoint>,
-        output: Vec<TxOut>,
-    },
-}
-
-#[near(serializers=[json])]
-#[derive(Debug, Clone)]
-pub struct UTXO {
-    pub path: String,
-    pub tx_bytes: Vec<u8>,
-    pub vout: usize,
-    #[serde(with = "u64_dec_format")]
-    pub balance: u64,
-}
+use near_sdk::serde::{Deserialize, Serialize};
 
 pub mod u64_dec_format {
     use near_sdk::serde::de;
@@ -41,4 +21,28 @@ pub mod u64_dec_format {
             .parse()
             .map_err(de::Error::custom)
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TokenReceiverMessage {
+    DepositProtocolFee,
+    Withdraw {
+        target_btc_address: String,
+        input: Vec<OutPoint>,
+        output: Vec<TxOut>,
+    },
+}
+
+#[near(serializers = [json])]
+#[derive(Clone)]
+#[cfg_attr(not(target_arch = "wasm32"), derive(Debug))]
+pub struct UTXO {
+    pub path: String,
+    pub tx_bytes: Vec<u8>,
+    pub vout: usize,
+    #[serde(
+    serialize_with = "u64_dec_format::serialize",
+    deserialize_with = "u64_dec_format::deserialize"
+    )]
+    pub balance: u64,
 }
