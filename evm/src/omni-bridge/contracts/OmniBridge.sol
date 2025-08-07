e4a0f3049043a8da42680cfe900ef561f052c5e5
karim-en <karim.alabtakh@nearone.org>
2025-08-07 17:56:44 +0100

    Add playable for `addCustomToken`

--- evm/src/omni-bridge/contracts/OmniBridge.sol
+++ evm/src/omni-bridge/contracts/OmniBridge.sol
@@ -66,7 +66,7 @@ contract OmniBridge is
         _grantRole(PAUSABLE_ADMIN_ROLE, _msgSender());
     }
 
-    function addCustomToken(string calldata nearTokenId, address tokenAddress, address customMinter, uint8 originDecimals) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function addCustomToken(string calldata nearTokenId, address tokenAddress, address customMinter, uint8 originDecimals) payable external onlyRole(DEFAULT_ADMIN_ROLE) {
         isBridgeToken[tokenAddress] = true;
         ethToNearToken[tokenAddress] = nearTokenId;
         nearToEthToken[nearTokenId] = tokenAddress;
