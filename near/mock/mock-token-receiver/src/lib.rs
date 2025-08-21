3ffe846afb93e96e0c9210726490e62c536a92d7
karim-en <karim.alabtakh@nearone.org>
2025-08-21 13:16:08 +0100

    Remove checks in mock

--- near/mock/mock-token-receiver/src/lib.rs
+++ near/mock/mock-token-receiver/src/lib.rs
@@ -1,7 +1,7 @@
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 
 use near_sdk::json_types::U128;
-use near_sdk::{env, near, serde_json, AccountId, PromiseOrValue};
+use near_sdk::{near, AccountId, PromiseOrValue};
 
 #[near(contract_state)]
 #[derive(Default)]
@@ -15,24 +15,6 @@ impl FungibleTokenReceiver for Contract {
         amount: U128,
         msg: String,
     ) -> PromiseOrValue<U128> {
-        env::log_str(&format!(
-            "ft_on_transfer called with sender_id: {}, amount: {}, msg: {}",
-            sender_id, amount.0, msg
-        ));
-
-        let msg = serde_json::from_str::<TestMessage>(&msg).unwrap();
-
-        if msg.panic {
-            env::panic_str("Panic from mock-token-receiver");
-        }
-
-        PromiseOrValue::Value(msg.return_value)
+        PromiseOrValue::Value(U128(0))
     }
-}
-
-#[near(serializers=[json])]
-struct TestMessage {
-    return_value: U128,
-    panic: bool,
-    extra_msg: String,
 }
