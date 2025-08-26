b392589c8eee22244fe881bb560f414b1568d0a0
Ivan Frolov <frolvanya@gmail.com>
2025-08-26 11:31:59 -0400

    docs: removed info about signer requirement for message field

--- solana/bridge_token_factory/programs/bridge_token_factory/src/instructions/wormhole_cpi.rs
+++ solana/bridge_token_factory/programs/bridge_token_factory/src/instructions/wormhole_cpi.rs
@@ -54,7 +54,7 @@ pub struct WormholeCPI<'info> {
     pub sequence: Account<'info, wormhole::SequenceTracker>,
 
     #[account(mut, seeds = [config.key().as_ref()], bump, seeds::program = wormhole_post_message_shim::ID)]
-    /// CHECK: Wormhole Message. [`wormhole::post_message`] requires this account be signer and mutable.
+    /// CHECK: Wormhole Message.
     /// Seeds constraint added for IDL generation / convenience, it will be enforced by the shim.
     pub message: UncheckedAccount<'info>,
 
