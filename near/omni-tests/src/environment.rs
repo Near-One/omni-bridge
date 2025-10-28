09e9a9d01293a29dbad9d098fc92d9aea86d1378
kiseln <3428059+kiseln@users.noreply.github.com>
2025-10-28 15:21:51 +0100

    Fix migration test

--- near/omni-tests/src/environment.rs
+++ near/omni-tests/src/environment.rs
@@ -19,7 +19,7 @@ use crate::helpers::tests::{
     get_test_deploy_token_args, BuildArtifacts, NEP141_DEPOSIT,
 };
 
-const PREV_LOCKER_WASM_FILEPATH: &str = "src/data/omni_bridge-0_2_17.wasm";
+const PREV_LOCKER_WASM_FILEPATH: &str = "src/data/omni_bridge-0_3_2.wasm";
 
 pub struct BridgeToken {
     pub is_deployed: bool,
