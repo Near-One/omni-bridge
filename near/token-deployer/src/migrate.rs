47d07e688cd5721a573814edff15c3d65f82616b
Ivan Frolov <frolvanya@gmail.com>
2025-12-04 18:59:16 -0500

    fix: migration

--- near/token-deployer/src/migrate.rs
+++ near/token-deployer/src/migrate.rs
@@ -3,6 +3,8 @@ use near_sdk::{collections::UnorderedSet, env, near, AccountId, PanicOnDefault, 
 
 use crate::{TokenDeployer, TokenDeployerExt};
 
+const STATE_KEY: &[u8] = b"STATE";
+
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct OldState {}
 
@@ -24,7 +26,15 @@ impl TokenDeployer {
     #[private]
     #[init(ignore_state)]
     pub fn migrate(omni_token_global_contract_id: AccountId) -> Self {
-        if env::state_read::<OldState>().is_some() || env::state_read::<OldLegacyState>().is_some()
+        if !env::state_exists() {
+            env::panic_str("Old state not found. Migration is not needed.")
+        }
+
+        let state = env::storage_read(STATE_KEY)
+            .unwrap_or_else(|| env::panic_str("Failed to read state key."));
+
+        if OldState::try_from_slice(&state).is_ok()
+            || OldLegacyState::try_from_slice(&state).is_ok()
         {
             Self {
                 omni_token_global_contract_id,
