5e401954713798c34fc0d4a89d27931bdeeca945
Ivan Frolov <frolvanya@gmail.com>
2026-01-30 16:48:48 +0100

    fix: lint

--- evm/src/common/IBridgeToken.sol
+++ evm/src/common/IBridgeToken.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.24;
 
 interface IBridgeToken {
     function mint(address beneficiary, uint256 amount) external;
-    function mintWithMsg(address beneficiary, uint256 amount, bytes memory message) external;
+    function mintWithMsg(
+        address beneficiary,
+        uint256 amount,
+        bytes memory message
+    ) external;
     function burn(address act, uint256 amount) external;
 }
