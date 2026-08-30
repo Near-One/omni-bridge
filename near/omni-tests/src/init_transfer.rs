60d510aa653b695c87a5bc958016f0bd8194c45d
Karim <karim.alabtakh@nearone.org>
2026-08-30 19:10:31 +0100

    chore: remove unused features (#639)
    * chore: unused features
    
    * Remove update fee tests
    
    * Fix clippy warrings
    
    * Simplify the pausing fast transfers
    
    * Remove extra macro
    
    * ignore fast trasnfers tests
    
    * fix tests

--- near/omni-tests/src/init_transfer.rs
+++ near/omni-tests/src/init_transfer.rs
@@ -9,7 +9,6 @@ mod tests {
     use omni_types::{
         near_events::OmniBridgeEvent, BoundedString, BridgeOnTransferMsg, ChainKind, Fee,
         InitTransferMsg, OmniAddress, TransferId, TransferMessage, TransferMessageStorageAccount,
-        UpdateFee,
     };
     use rstest::rstest;
 
@@ -78,7 +77,6 @@ mod tests {
         transfer_amount: u128,
         init_transfer_msg: InitTransferMsg,
         custom_deposit: Option<NearToken>,
-        update_fee: Option<UpdateFee>,
         is_relayer_sign: bool,
     ) -> anyhow::Result<()> {
         let storage_deposit_amount = get_balance_required_for_account(
@@ -119,23 +117,7 @@ mod tests {
 
         // Ensure the transfer event is emitted
         let transfer_message = get_transfer_message_from_event(&transfer_result)?;
-
-        // Update the transfer fee if needed
-        let signing_fee = if let Some(update_fee) = update_fee.clone() {
-            make_fee_update(
-                update_fee.clone(),
-                &transfer_message,
-                &env.locker_contract,
-                &env.sender_account,
-            )
-            .await?;
-            match update_fee {
-                UpdateFee::Fee(new_fee) => new_fee,
-                UpdateFee::Proof(_) => transfer_message.fee.clone(),
-            }
-        } else {
-            transfer_message.fee.clone()
-        };
+        let signing_fee = transfer_message.fee.clone();
 
         // Transfer is signed by the relayer or the sender
         let signer = if is_relayer_sign {
@@ -262,39 +244,6 @@ mod tests {
         Ok(transfer_message)
     }
 
-    async fn make_fee_update(
-        update_fee: UpdateFee,
-        transfer_message: &TransferMessage,
-        locker_contract: &near_workspaces::Contract,
-        sender_account: &near_workspaces::Account,
-    ) -> anyhow::Result<()> {
-        let deposit = match update_fee.clone() {
-            UpdateFee::Fee(update_fee) => NearToken::from_yoctonear(
-                update_fee
-                    .native_fee
-                    .0
-                    .saturating_sub(transfer_message.fee.native_fee.0),
-            ),
-            // To be updated once the proof is implemented
-            UpdateFee::Proof(_) => NearToken::from_yoctonear(0),
-        };
-        sender_account
-            .call(locker_contract.id(), "update_transfer_fee")
-            .args_json(json!({
-                "transfer_id": TransferId {
-                    origin_chain: ChainKind::Near,
-                    origin_nonce: transfer_message.origin_nonce,
-                },
-                "fee": update_fee.clone(),
-            }))
-            .max_gas()
-            .deposit(deposit)
-            .transact()
-            .await?
-            .into_result()?;
-        Ok(())
-    }
-
     async fn get_token_balance(
         token_contract: &near_workspaces::Contract,
         account_id: &AccountId,
@@ -341,15 +290,8 @@ mod tests {
 
         let env = TestEnv::new(sender_balance_token, false, build_artifacts).await?;
 
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            None,
-            true,
-        )
-        .await?;
+        init_transfer_flow_on_near(&env, transfer_amount, init_transfer_msg.clone(), None, true)
+            .await?;
 
         let (user_balance_token, locker_balance_token, _, relayer_balance_near) =
             get_test_balances(&env).await?;
@@ -389,15 +331,8 @@ mod tests {
 
         let env = TestEnv::new(sender_balance_token, false, build_artifacts).await?;
 
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            None,
-            true,
-        )
-        .await?;
+        init_transfer_flow_on_near(&env, transfer_amount, init_transfer_msg.clone(), None, true)
+            .await?;
 
         let (user_balance_token, locker_balance_token, relayer_balance_token, _) =
             get_test_balances(&env).await?;
@@ -435,15 +370,8 @@ mod tests {
 
         let env = TestEnv::new(sender_balance_token, false, build_artifacts).await?;
 
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            None,
-            true,
-        )
-        .await?;
+        init_transfer_flow_on_near(&env, transfer_amount, init_transfer_msg.clone(), None, true)
+            .await?;
 
         let (user_balance_token, locker_balance_token, relayer_balance_token, relayer_balance_near) =
             get_test_balances(&env).await?;
@@ -477,66 +405,6 @@ mod tests {
 
     #[rstest]
     #[tokio::test]
-    async fn test_update_fee(build_artifacts: &BuildArtifacts) -> anyhow::Result<()> {
-        let sender_balance_token = 1_000_000;
-        let transfer_amount = 5000;
-        let init_transfer_msg = InitTransferMsg {
-            native_token_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(1000),
-            recipient: eth_eoa_address(),
-            msg: None,
-            external_id: None,
-        };
-        let update_fee_value = Fee {
-            native_fee: U128(NearToken::from_near(2).as_yoctonear()),
-            fee: U128(2000),
-        };
-        let update_fee = UpdateFee::Fee(update_fee_value.clone());
-
-        let env = TestEnv::new(sender_balance_token, false, build_artifacts).await?;
-
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            Some(update_fee),
-            true,
-        )
-        .await?;
-
-        let (user_balance_token, locker_balance_token, relayer_balance_token, relayer_balance_near) =
-            get_test_balances(&env).await?;
-
-        assert_eq!(
-            user_balance_token,
-            U128(sender_balance_token - transfer_amount),
-            "User balance was not deducted"
-        );
-        assert_eq!(
-            locker_balance_token,
-            U128(transfer_amount - update_fee_value.fee.0),
-            "Locker balance was not increased or the fee was not deducted"
-        );
-
-        assert!(
-            DEFAULT_NEAR_SANDBOX_BALANCE.as_yoctonear()
-                + NearToken::from_yoctonear(update_fee_value.native_fee.0).as_yoctonear()
-                - relayer_balance_near.as_yoctonear()
-                < EXPECTED_RELAYER_GAS_COST.as_yoctonear(),
-            "Relayer didn't receive native fee."
-        );
-
-        assert_eq!(
-            relayer_balance_token,
-            U128(update_fee_value.fee.0),
-            "Relayer didn't receive transfer fee."
-        );
-        Ok(())
-    }
-
-    #[rstest]
-    #[tokio::test]
     async fn test_relayer_sign(build_artifacts: &BuildArtifacts) -> anyhow::Result<()> {
         let sender_balance_token = 1_000_000;
         let transfer_amount = 100;
@@ -550,15 +418,8 @@ mod tests {
 
         let env = TestEnv::new(sender_balance_token, false, build_artifacts).await?;
 
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            None,
-            true,
-        )
-        .await?;
+        init_transfer_flow_on_near(&env, transfer_amount, init_transfer_msg.clone(), None, true)
+            .await?;
 
         let (user_balance_token, locker_balance_token, _, relayer_balance_near) =
             get_test_balances(&env).await?;
@@ -851,143 +712,6 @@ mod tests {
         );
 
         Ok(())
-    }
-
-    #[rstest]
-    #[tokio::test]
-    #[should_panic(expected = "ERR_LOWER_FEE")]
-    async fn test_update_fee_native_too_small(build_artifacts: &BuildArtifacts) {
-        let sender_balance_token = 1_000_000;
-        let transfer_amount = 5000;
-        let init_transfer_msg = InitTransferMsg {
-            native_token_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(1000),
-            recipient: eth_eoa_address(),
-            msg: None,
-            external_id: None,
-        };
-        let update_fee_value = Fee {
-            native_fee: U128(NearToken::from_near(0).as_yoctonear()),
-            fee: U128(2000),
-        };
-        let update_fee = UpdateFee::Fee(update_fee_value.clone());
-
-        let env = TestEnv::new(sender_balance_token, false, build_artifacts)
-            .await
-            .unwrap();
-
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            Some(update_fee),
-            false,
-        )
-        .await
-        .unwrap();
-    }
-
-    #[rstest]
-    #[tokio::test]
-    #[should_panic(expected = "ERR_INVALID_FEE")]
-    async fn test_update_fee_transfer_fee_too_small(build_artifacts: &BuildArtifacts) {
-        let sender_balance_token = 1_000_000;
-        let transfer_amount = 5000;
-        let init_transfer_msg = InitTransferMsg {
-            native_token_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(1000),
-            recipient: eth_eoa_address(),
-            msg: None,
-            external_id: None,
-        };
-        let update_fee_value = Fee {
-            native_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(500),
-        };
-        let update_fee = UpdateFee::Fee(update_fee_value.clone());
-
-        let env = TestEnv::new(sender_balance_token, false, build_artifacts)
-            .await
-            .unwrap();
-
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            Some(update_fee),
-            false,
-        )
-        .await
-        .unwrap();
-    }
-
-    #[rstest]
-    #[tokio::test]
-    #[should_panic(expected = "ERR_INVALID_FEE")]
-    async fn test_update_fee_transfer_fee_too_big(build_artifacts: &BuildArtifacts) {
-        let sender_balance_token = 1_000_000;
-        let transfer_amount = 5000;
-        let init_transfer_msg = InitTransferMsg {
-            native_token_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(1000),
-            recipient: eth_eoa_address(),
-            msg: None,
-            external_id: None,
-        };
-        let update_fee_value = Fee {
-            native_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(6000),
-        };
-        let update_fee = UpdateFee::Fee(update_fee_value.clone());
-
-        let env = TestEnv::new(sender_balance_token, false, build_artifacts)
-            .await
-            .unwrap();
-
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            Some(update_fee),
-            false,
-        )
-        .await
-        .unwrap();
-    }
-
-    #[rstest]
-    #[tokio::test]
-    #[should_panic(expected = "ERR_UNSUPPORTED_FEE_UPDATE_PROOF")]
-    // Add a test once the Proof update fee is implemented
-    async fn test_update_fee_proof(build_artifacts: &BuildArtifacts) {
-        let sender_balance_token = 1_000_000;
-        let transfer_amount = 5000;
-        let init_transfer_msg = InitTransferMsg {
-            native_token_fee: U128(NearToken::from_near(1).as_yoctonear()),
-            fee: U128(1000),
-            recipient: eth_eoa_address(),
-            msg: None,
-            external_id: None,
-        };
-        let update_fee = UpdateFee::Proof(vec![]);
-
-        let env = TestEnv::new(sender_balance_token, false, build_artifacts)
-            .await
-            .unwrap();
-
-        init_transfer_flow_on_near(
-            &env,
-            transfer_amount,
-            init_transfer_msg.clone(),
-            None,
-            Some(update_fee),
-            false,
-        )
-        .await
-        .unwrap();
     }
 
     #[rstest]
